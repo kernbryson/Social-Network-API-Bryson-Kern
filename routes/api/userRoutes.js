24f090312ec13f7637c05e8f1c814def62654960
Bryson Kern <kernbryson@yahoo.com>
2022-07-07 15:18:31 -0400

    fixed route and controller hopefully

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -14,6 +14,6 @@ router.route("/").get(getUsers).post(createUser);
 // /api/users/:userId
 router.route("/:userId").get(getSingleUser).delete(deleteUser).put(updateUser);
 
-router.route("/:id/friends/:friendId").post(addFriend);
+router.route("/:userId/friends/:friendId").post(addFriend);
 
 module.exports = router;
